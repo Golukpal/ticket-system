5edf09a1e137ebb442b98caf456cc07c5d0c2cb0
Golu Kumar Pal <golukpal@ssipmt.com>
2026-09-24 00:41:35 +0530

    update routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -5,12 +5,20 @@ import (
 	"net/http"
 
 	"github.com/Golukpal/ticket-system/internal/handler"
+	"github.com/Golukpal/ticket-system/internal/middleware"
 )
 
-func Setup(authHandler *handler.AuthHandler) http.Handler {
+func Setup(
+	authHandler *handler.AuthHandler,
+	ticketHandler *handler.TicketHandler,
+	authMiddleware *middleware.AuthMiddleware,
+) http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /health", healthHandler)
+	mux.HandleFunc(
+		"GET /health",
+		healthHandler,
+	)
 
 	mux.HandleFunc(
 		"POST /auth/register",
@@ -20,6 +28,13 @@ func Setup(authHandler *handler.AuthHandler) http.Handler {
 	mux.HandleFunc(
 		"POST /auth/login",
 		authHandler.Login,
+	)
+
+	mux.Handle(
+		"POST /tickets",
+		authMiddleware.RequireAuth(
+			http.HandlerFunc(ticketHandler.Create),
+		),
 	)
 
 	return mux
